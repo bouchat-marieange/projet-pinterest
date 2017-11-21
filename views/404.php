92f1126999c608886887a0228be3de97f8c3ef60
Vincent <v.nassaux@nootos.com>
2017-11-21 16:16:31 +0100

    Revert "Preparation Error 404"
    This reverts commit 268b732952e2461600eb900476be6198d7db79a2.

--- views/404.php
+++ views/404.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <!doctype html>
 <html lang="en">
 
@@ -15,26 +14,3 @@
   </body>
 
 </html>
-=======
-<!-- 1e possibilité -->
-
-<?php
-
-// Get the current response code and set a new one
-var_dump(http_response_code(Erreur 404));
-
-// Get the new response code
-var_dump(http_response_code());
-
-?>
-
-
-
-<!-- 2e possibilité : -->
-
-<?php
-
-   echo "La page n'existe pas ou plus !";
-
-?>
->>>>>>> Stashed changes
