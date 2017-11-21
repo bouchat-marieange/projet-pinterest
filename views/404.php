268b732952e2461600eb900476be6198d7db79a2
Vincent <v.nassaux@nootos.com>
2017-11-21 16:16:31 +0100

    Preparation Error 404

--- views/404.php
+++ views/404.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!doctype html>
 <html lang="en">
 
@@ -14,3 +15,26 @@
   </body>
 
 </html>
+=======
+<!-- 1e possibilité -->
+
+<?php
+
+// Get the current response code and set a new one
+var_dump(http_response_code(Erreur 404));
+
+// Get the new response code
+var_dump(http_response_code());
+
+?>
+
+
+
+<!-- 2e possibilité : -->
+
+<?php
+
+   echo "La page n'existe pas ou plus !";
+
+?>
+>>>>>>> Stashed changes
