3be474822c74397e5fb581a416bfed8126e8144a
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-22 15:58:17 +0100

    Modif chemin css dans assets views, et transfert dossiers js et bootstrap dans assets views

--- views/header_main_disconnect.php
+++ views/header_main_disconnect.php
@@ -7,7 +7,7 @@
     <!-- Link vers bootstrap à placer avant tout autres feuille de CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <!-- Link vers notre feuille css -->
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/header.css">
     <title>header_main_disconnect</title>
   </head>
 
