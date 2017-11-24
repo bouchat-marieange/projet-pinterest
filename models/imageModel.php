1efea282122d456d91c4c7eb2443cbc7469a914d
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-11-24 13:52:10 +0100

    Wip on imageModel

---
+++ models/imageModel.php
@@ -0,0 +1,23 @@
+<?php
+
+class ImageModel
+{
+
+    private $db;
+
+    public function __construct()
+    {
+
+        try {
+            $this->db = new PDO("mysql:host=localhost;dbname=pinterest", "root", "root");
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (Exception $exception) {
+            die($exception);
+        }
+    }
+    public function add($titre, $image, $description, $date)
+    {
+        $req = $this->db->prepare("INSERT INTO images (titre, image, description, d) VALUES (?,?)");
+        $req->execute([$pseudo, $password]);
+    }
