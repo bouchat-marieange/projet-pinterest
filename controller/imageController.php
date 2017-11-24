69ccf30b264a3cdc0d4d3b152387df43c7d7d81c
user <kevin.flbt@gmail.com>
2017-11-24 14:08:34 +0100

    commit 2 test

--- controller/imageController.php
+++ controller/imageController.php
@@ -1,35 +1,6 @@
 
 <?php
 
-class ImageController{
-
-
-  public $imageModel;
-  public function __construct(){
-
-      {
-
-            try {
-                $this->db = new PDO("mysql:host=10.20.1.23;dbname=pinterest", "root", "user");
-                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            } catch (Exception $exception) {
-                die($exception);
-            }
-        }
-    }
-  }
-
-
-public function
-
-
-}
-
-
-
-
-
 
 
 
--- models/UserModel.php
+++ models/UserModel.php
@@ -9,7 +9,7 @@ class UserModel
     {
 
         try {
-            $this->db = new PDO("mysql:host=localhost;dbname=pinterest", "root", "root");
+            $pdo = new PDO('pgsql:host=ec2-46-137-174-67.eu-west-1.compute.amazonaws.com;dbname=d583l7ucmi14j1;', 'whcxkhxgfgjals','142f864119a26f291318da08efdf2af4f656849227e3d679118eaf99f22681bb');
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         } catch (Exception $exception) {
