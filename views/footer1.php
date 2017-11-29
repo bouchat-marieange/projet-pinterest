a6baa9c8b9319f17cd39dcffa128f97e73247f93
Vincent <v.nassaux@nootos.com>
2017-11-29 15:02:12 +0100

    Commit new footer1.php
    ajout du nouveau footer avec css

---
+++ views/footer1.php
@@ -0,0 +1,70 @@
+<?php
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+    <!-- Link vers notre feuille css -->
+    <link rel="stylesheet" href="./assets/css/footer1.css">
+
+    <!-- Link vers notre feuille css de bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <title>footer</title>
+  </head>
+
+  <body>
+<footer>
+    <div id="footer" class="container">  <!-- align-items-end -->
+
+      <div class="row ">
+
+        <div class="col col-sm-6 col-lg-4">
+          <div class="collaborateurs">
+            <h6>COLLABORATEURS</h6>
+            <div class="row">
+              <a class="w-50 p-0 btn btn-primary" href="https://github.com/bouchat-marieange" role="button"><i class="fa fa-github" aria-hidden="true"></i> MARIE-ANGE</br>BOUCHAT</a>    <!-- pour faire le bouton qui amène au Github de M-A.B -->
+              <a class="w-50 p-0 btn btn-primary" href="https://github.com/Nicopaku" role="button"><i class="fa fa-github" aria-hidden="true"></i> NICOLAS</br>BELPOIS</a>       <!-- pour faire le bouton qui amène au Github de N.B. -->
+              <!-- <div class="w-100"></div> -->
+              <a class="w-50 p-0 btn btn-primary" href="https://github.com/keloriane" role="button"><i class="fa fa-github" aria-hidden="true"></i> KEVIN</br>FLABAS</a>          <!-- pour faire le bouton qui amène au Github de K.F. -->
+              <a class="w-50 p-0 btn btn-primary" href="https://github.com/LoutreT" role="button"><i class="fa fa-github" aria-hidden="true"></i> VINCENT</br>NASSAUX</a>       <!-- pour faire le bouton qui amène au Github de V.N. -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col col-sm-6 col-lg-4">
+          <div class="Becentral">
+            <h6>BECODE  BXL-CENTRAL</h6>
+            <div class="Adresse">
+              <a class="h-100 w-100 p-3 btn btn-primary" href="http://www.becode.org/" role="button">WWW.BECODE.ORG</a>  <!-- pour faire le bouton web -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col col-sm-6 col-lg-4">
+          <div class="Repository">
+            <h6>REPOSITORY</h6>
+            <div class="REPOS">
+             <a class="h-100 w-100 p-3 btn btn-primary" href="https://github.com/bouchat-marieange/projet-pinterest" role="button"><i class="fa fa-github" aria-hidden="true"></i> REPO. DU PROJET</a>
+            <!-- <a href="https://github.com/bouchat-marieange/projet-pinterest"><h6>
+            <i class="fa fa-github" aria-hidden="true"></i> REPOSITORY DU PROJET</h6></a> -->
+            </div>
+            <div class="Copyright">
+              <p> <!-- logo copyright --> Powered by VIKENIMA - 2017 <br>For Becode.org</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</footer>
+  </body>
+
+</html>
